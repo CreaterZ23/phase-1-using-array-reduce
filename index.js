4faac7fc9c2455e3586a3ac367953db42ef2f9bc
Zeon <zeonstewart23@gmail.com>
2021-08-13 22:57:57 -0400

    commit tree bc9cb9fa20fe24695717f87af55c7f8247507ffb

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// const totalBatteries = batteryBatches.reduce(function (allBatteries, batches) {
+//     return allBatteries + batches;
+// }, 0);
+
+const totalBatteries = batteryBatches.reduce((allBatteries, batches) => allBatteries + batches
+,0);
+
+console.log(totalBatteries);
